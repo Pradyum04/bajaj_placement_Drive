739c8a8a76896fc44b3d0a0cad1d6697cbad4f42
pradyum_chopade <112606679+Pradyum04@users.noreply.github.com>
2025-03-27 16:23:43 +0530

    Add files via upload

---
+++ Gson.java
@@ -0,0 +1,9 @@
+
+public class Gson {
+
+    public StudentData fromJson(String jsonContent, Class<StudentData> class1) {
+    
+        throw new UnsupportedOperationException("Unimplemented method 'fromJson'");
+    }
+
+}
